60718cd416256665807881d14b8035efddd64f64
Benjamin <gyamfibenjamin135@gmail.com>
2023-02-16 16:36:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main -Programming Hello with printf
+ *Return:(0)Success
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);		
+}
