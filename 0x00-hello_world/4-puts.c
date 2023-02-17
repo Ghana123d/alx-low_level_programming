5100839778c36147eae85048d61758c80319bd71
Benjamin <gyamfibenjamin135@gmail.com>
2023-02-16 16:23:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main-Programming for hello puts
+ * main -Programming for hello puts
  * Return:(0)Success
  */
-int main (void)
+int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
