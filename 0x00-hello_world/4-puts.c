d64f77d13ddafe2489942b8cdf499675c8bdc7cb
Benjamin <gyamfibenjamin135@gmail.com>
2023-02-16 16:16:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main (Hello, puts)
  * Returm(0)=Success
  */
 int main (void)
 {	
-	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
